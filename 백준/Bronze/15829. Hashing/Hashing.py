1d507c189318f58eabdc990c88a81cc4d36a1756
YoonGeonWook <108259316+YoonGeonWook@users.noreply.github.com>
2025-04-04 16:34:22 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def hash(L, string):
+	M = 1234567891
+	result = 0
+	for l in range(L):
+		result += (ord(string[l])-96) * 31**l
+	return result
+
+# 입력
+L = int(input())
+string = input()
+
+# 출력
+print(hash(L, string))
