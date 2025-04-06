a78c44a5ddf0d59aa86c7712018e9ec4302434e3
YoonGeonWook <108259316+YoonGeonWook@users.noreply.github.com>
2025-04-06 23:25:54 +0900

    [Silver V] Title: 나이순 정렬, Time: 240 ms, Memory: 46020 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,14 @@
+import sys 
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+members = []
+for _ in range(N):
+	age, name = input().split()
+	age = int(age)
+	members.append((age, name))
+
+members.sort(key=lambda x: x[0])
+
+for age, name in members:
+	print(age, name)
