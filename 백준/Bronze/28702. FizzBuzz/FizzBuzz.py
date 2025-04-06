ae0f50130ea88952c6816679439f537c36d52c6d
YoonGeonWook <108259316+YoonGeonWook@users.noreply.github.com>
2025-04-06 22:35:31 +0900

    [Bronze I] Title: FizzBuzz, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -1,29 +1,23 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
 def fizzbuzz(i):
-    if i % 3 == 0 and i % 5 == 0:
-        return "FizzBuzz"
-    elif i % 3 == 0:
-        return "Fizz"
-    elif i % 5 == 0:
-        return "Buzz"
-    else:
-        return str(i)
+	if i%3==0 and i%5==0:
+		return "FizzBuzz"
+	elif i%3==0:
+		return "Fizz"
+	elif i%5==0:
+		return "Buzz"
+	else:
+		return str(i)
 
 # 입력
-seq = [input().strip() for _ in range(3)]
+seq = [input() for _ in range(3)]
 
-# 숫자가 있다면 추정 시작점 찾아서 시도해보기 (최대 3번)
 for idx, val in enumerate(seq):
-    if val.isdigit():
-        possible_start = int(val) - idx
-        for i in range(possible_start - 1, possible_start + 2):  # 여유 범위
-            if [fizzbuzz(i), fizzbuzz(i+1), fizzbuzz(i+2)] == seq:
-                print(fizzbuzz(i+3))
-                exit()
+	if val.isdigit():
+		i = int(val) - idx
+		if [fizzbuzz(i), fizzbuzz(i+1), fizzbuzz(i+2)] == seq:
+			print(fizzbuzz(i+3))
+			break
 
-# 숫자가 없다면 fallback: 1부터 탐색 (필요하면)
-i = 1
-while True:
-    if [fizzbuzz(i), fizzbuzz(i+1), fizzbuzz(i+2)] == seq:
-        print(fizzbuzz(i+3))
-        break
-    i += 1
