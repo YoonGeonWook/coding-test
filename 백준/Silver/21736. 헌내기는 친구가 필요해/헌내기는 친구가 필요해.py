3400943672f7560395753cb274a8c8d935eb00c2
YoonGeonWook <108259316+YoonGeonWook@users.noreply.github.com>
2025-04-11 14:46:31 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 384 ms, Memory: 41388 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,27 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-sys.setrecursionlimit(1000000)
+from collections import deque
 
-def dfs(x, y):
-	global people # 해당 값을 수정해야 하므로 전역변수 지정
-	visited[x][y] = True # 현재 위치 방문 처리
+def bfs(x, y):
+	queue = deque([(x, y)])
+	visited[x][y] = True
 
-	if coords[x][y] == 'P':
-		people += 1
+	people = 0
 
-	for d in range(4):
-		nx = x + dx[d]
-		ny = y + dy[d]
+	while queue:
+		x, y = queue.popleft()
 
-		if 0 <= nx < N and 0 <= ny < M:
-			if not visited[nx][ny] and coords[nx][ny] != 'X':
-				dfs(nx, ny)
+		for d in range(4):
+			nx = x + dx[d]
+			ny = y + dy[d]
+
+			if 0 <= nx < N and 0 <= ny < M:
+				if not visited[nx][ny] and coords[nx][ny] != 'X':
+					visited[nx][ny] = True
+					queue.append((nx, ny))
+					if coords[nx][ny] == 'P':
+						people += 1
+	return people
 
 N, M = map(int, input().split())
-# coords = []
-# for _ in range(N):
-# 	coords.append(list(input().strip()))
 coords = [list(input().strip()) for _ in range(N)]
 
 # 방향 벡터: 상/하/좌/우
@@ -30,13 +33,13 @@ dy = [-1, 1, 0, 0]
 
 # 방문 여부
 visited = [[False] * M for _ in range(N)]
-people = 0
 
 # 도연이 위치 찾기 및 DFS 시작
 for i in range(N):
 	for j in range(M):
 		if coords[i][j] == 'I':
-			dfs(i, j)
+			start = (i, j)
 			break
 
-print(people if people > 0 else 'TT')
+result = bfs(start[0], start[1])
+print(result if result > 0 else 'TT')
