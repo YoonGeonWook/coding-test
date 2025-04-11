3318053f83f5d57be9d6c43b252dacf053bd7fc6
YoonGeonWook <108259316+YoonGeonWook@users.noreply.github.com>
2025-04-11 18:13:35 +0900

    [Silver I] Title: IOIOI, Time: 308 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,10 +5,21 @@ N = int(input())
 M = int(input())
 S = input().strip()
 
-P = 'IO' * N + 'I'
-
+i = 0
+answer = 0
 cnt = 0
-for start_idx in range(M):
-	if S[start_idx:start_idx+len(P)] == P:
-		cnt += 1
-print(cnt)
+
+while i < M - 1:
+    # 'IOI' 패턴 발견 시
+    if S[i:i+3] == 'IOI':
+        cnt += 1
+        i += 2  # 'IOI'는 두 칸 이동
+        # N개 이상 반복되면 P_N 패턴이 되는 것
+        if cnt >= N:
+            answer += 1
+    else:
+        # 패턴이 끊기면 초기화
+        cnt = 0
+        i += 1
+
+print(answer)
